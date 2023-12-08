b6472490291cf560b77ac25993ce5c4b7e312b59
matv864 <matv864@gmail.com>
2023-12-08 15:50:04 +1000

    schema is built

--- src/entities/schema/paint_side.py
+++ src/entities/schema/paint_side.py
@@ -21,41 +21,46 @@ class Painting:
         self.painting()
 
     def painting(self):
-        for obj in self.game_config.level_config["logic_objects"].values():
-            type_logic = obj["type"]
-            if type_logic == "splitter":
-                for next_id in obj["turn_object"]:
-                    self.paint_logic_object(obj, next_id)
-            else:
-                self.paint_logic_object(obj, obj["turn_object"])
-
         for obj in self.game_config.level_config["platforms"]:
             self.paint_wire_from_platforms(obj, obj["activate_from_obj_id"])
 
         for obj in self.game_config.level_config["levers"]:
             self.paint_wire_from_levers(obj, obj["turn_object"])
 
+        for obj in self.game_config.level_config["logic_objects"].values():
+            type_logic = obj["type"]
+            
+            if type_logic == "splitter":
+                for next_id in obj["turn_object"]:
+                    self.paint_logic_wire(obj, next_id)
+            else:
+                self.paint_logic_wire(obj, obj["turn_object"])
+
+            self.paint_logic_object(obj)
+
         self.game_config.screen.blit(self.main_surf, self.game_config.schema_module_location)
 
-    def paint_logic_object(self, obj, next_id):
-        surf = pygame.Surface((self.size_logic_x, self.size_logic_y))
-        surf.fill((100, 50, 200))
-        self.main_surf.blit(surf, (obj["x"] * self.vw, obj["y"] * self.vh))
+    def paint_logic_object(self, obj):
+
+        type_obj = obj["type"]
+        match type_obj:
+            case "node":
+                self.paint_node(obj["x"], obj["y"], obj["result_signal"])
+            case "splitter":
+                self.paint_node(obj["x"], obj["y"], obj["result_signal"])
+            case _:
+                surf = pygame.Surface((self.size_logic_x, self.size_logic_y))
+                surf.fill((100, 50, 200))
+                self.main_surf.blit(surf, (obj["x"] * self.vw, obj["y"] * self.vh))
+
+    def paint_logic_wire(self, obj, next_id):
+        # paint line
         if next_id != FALSE_ID:
             # start_pos = (obj["x"] * self.vw, obj["y"] * self.vh)
             next_x = self.game_config.level_config["logic_objects"][str(next_id)]["x"]
             next_y = self.game_config.level_config["logic_objects"][str(next_id)]["y"]
-            if obj["y"] == next_y:
-                start_pos = (obj["x"] * self.vw + self.size_logic_x, obj["y"] * self.vh + self.size_logic_y / 2)
-                end_pos = (next_x * self.vw + self.size_logic_x, next_y * self.vh + self.size_logic_y / 2)
-            elif obj["x"] == next_x:
-                start_pos = (obj["x"] * self.vw + self.size_logic_x / 2, obj["y"] * self.vh + self.size_logic_y)
-                end_pos = (next_x * self.vw + self.size_logic_x / 2, next_y * self.vh + self.size_logic_y)
-            else:
-                # print("diagonal")
-                # print(obj["x"], obj["y"], next_x, next_y)
-                start_pos = (obj["x"] * self.vw + self.size_logic_x / 2, obj["y"] * self.vh + self.size_logic_y / 2)
-                end_pos = (next_x * self.vw + self.size_logic_x / 2, next_y * self.vh + self.size_logic_y / 2)
+            start_pos = (obj["x"] * self.vw + self.size_logic_x / 2, obj["y"] * self.vh + self.size_logic_y / 2)
+            end_pos = (next_x * self.vw + self.size_logic_x / 2, next_y * self.vh + self.size_logic_y / 2)
             
             if obj["result_signal"]:
                 color = (0, 250, 0)
@@ -67,7 +72,7 @@ class Painting:
     def paint_wire_from_platforms(self, obj, next_id):
         start_x = self.game_config.level_config["logic_objects"][str(next_id)]["x"]
         start_y = self.game_config.level_config["logic_objects"][str(next_id)]["y"]
-        start_pos = (start_x * self.vw, start_y * self.vh + self.size_logic_y / 2)
+        start_pos = (start_x * self.vw + self.size_logic_x / 2, start_y * self.vh + self.size_logic_y / 2)
         end_pos = (100 * self.vw, obj["y"] * self.vh + self.size_logic_y / 2)
         if obj["activated"]:
             color = (0, 250, 0)
@@ -79,7 +84,7 @@ class Painting:
         end_x = self.game_config.level_config["logic_objects"][str(next_id)]["x"]
         end_y = self.game_config.level_config["logic_objects"][str(next_id)]["y"]
         start_pos = (0, obj["y"] * self.vh + self.size_logic_y / 2)
-        end_pos = (end_x * self.vw, end_y * self.vh + self.size_logic_y / 2)
+        end_pos = (end_x * self.vw + self.size_logic_x / 2, end_y * self.vh + self.size_logic_y / 2)
         
         if obj["activated"]:
             color = (0, 250, 0)
@@ -87,4 +92,12 @@ class Painting:
             color = (0, 0, 0)
         pygame.draw.line(self.main_surf, color, start_pos, end_pos, 1)
 
-        
+
+# painting logic object
+    def paint_node(self, x, y, activated):
+        if activated:
+            color = (0, 250, 0)
+        else:
+            color = (0, 0, 0)
+        center_pos = (x * self.vw + self.size_logic_x / 2, y * self.vh + self.size_logic_y / 2)
+        pygame.draw.circle(self.main_surf, color, center_pos, 5)
